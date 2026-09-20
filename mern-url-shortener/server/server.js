80d30bf60d02e1b5129b82be6727703654f1f778
Jonty Gupta <jontygupta010204@gmail.com>
2026-09-20 19:32:31 +0530

    [fix] : connection issue fixed

--- mern-url-shortener/server/server.js
+++ mern-url-shortener/server/server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.set('trust proxy', 1);
+
 // Database Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
